4c37be06236103d0dc46817caa7467abcc53ac63
Locomotive2te116 <locomotive2te116@gmail.com>
2023-12-15 13:05:29 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,21 @@
+import { Link, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import Home from 'pages/Home';
+import Movies from 'pages/Movies';
+import NotFound from 'pages/NotFound';
+
 export const App = () => {
-  return <div>React homework template</div>;
+  return (
+    <>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/movies">Movies</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 };
